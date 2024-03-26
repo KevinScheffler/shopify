151c56bbf3c2fc6f2fd2a47b71975c913151b65f
kevinscheffler <schefk2122@gmail.com>
2024-03-26 16:24:32 -0400

    Finished countdown timer

--- assets/global.js
+++ assets/global.js
@@ -1051,3 +1051,41 @@ class ProductRecommendations extends HTMLElement {
 }
 
 customElements.define('product-recommendations', ProductRecommendations);
+
+
+
+
+// Custom JavaScript for countdown timer
+document.addEventListener('DOMContentLoaded', function() {
+  var countdownTimer = setInterval(function () {
+
+    var countdownDate = document.getElementById('countdown').getAttribute('data-countdown-date');
+    var timer = document.getElementById('countdown');
+  
+    var endTime = new Date(countdownDate).getTime();
+    var currentTime = new Date().getTime();
+  
+    var timeRemaining = endTime - currentTime;
+  
+    var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    if (timeRemaining > 0) {
+      timer.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    }
+
+  
+    if (timeRemaining < 0) {
+      clearInterval(countdownTimer);
+      countdownDate.innerHTML = "EXPIRED";
+    }
+  }, 1000)
+});
+
+
+  
+
+  
+
